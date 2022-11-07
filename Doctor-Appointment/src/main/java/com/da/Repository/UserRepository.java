6cac7fdb8d2e574007e745d5ab9ec48fabeab4d7
Shodh yadav <112282005+shodhyadav@users.noreply.github.com>
2022-11-07 18:27:54 +0530

    Delete Doctor-Appointment directory

--- Doctor-Appointment/src/main/java/com/da/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.da.Repository;
-
-import com.da.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByUsername(String username);
-    public User findById(int id);
-}
