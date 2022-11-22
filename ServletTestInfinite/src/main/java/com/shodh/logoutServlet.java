81db7a382443393c532f0db2da0971e072a7f981
Shodh yadav <112282005+shodhyadav@users.noreply.github.com>
2022-11-22 17:11:49 +0530

    Servlet Test

---
+++ ServletTestInfinite/src/main/java/com/shodh/logoutServlet.java
@@ -0,0 +1,29 @@
+package com.shodh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class logoutServlet extends HttpServlet {
+   @Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	   response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		
+		request.getRequestDispatcher("link.html").include(request, response);
+		
+		HttpSession session=request.getSession();
+		session.invalidate();
+		
+		out.print("You are successfully logged out!");
+		
+		out.close();
+}
+}
